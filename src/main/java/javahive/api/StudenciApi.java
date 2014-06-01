218df45c2c878c28270891b73fe829106bc5e28e
krzysztof-osiecki <krzysztof@live.fr>
2014-06-01 15:07:24 +0200

    Zastosowanie memento w StudenciApi.java

--- src/main/java/javahive/api/StudenciApi.java
+++ src/main/java/javahive/api/StudenciApi.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javahive.api.dto.StudentDTO;
+import javahive.api.dto.StudentDTOMementoCaretaker;
 import javahive.api.dto.WykladDTO;
 import javahive.api.dto.ZaliczenieDTO;
 import javahive.domain.Student;
@@ -26,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class StudenciApi {
     @Inject
     private Finder finder;
+    private StudentDTOMementoCaretaker mementoCaretaker;
     public List<StudentDTO> getListaWszystkichStudentow() {
         List studentciDTO=new ArrayList<StudentDTO>();
         for(Student student: finder.findAll(Student.class)) {
@@ -36,6 +38,7 @@ public class StudenciApi {
             .indeks(student.getIndeks())
             .id(student.getId())
             .buduj();
+            mementoCaretaker.setMemento(studentDTO.createMemento());
             studentciDTO.add(studentDTO);
         }
         return studentciDTO;
@@ -68,6 +71,7 @@ public class StudenciApi {
         .indeks(student.getIndeks())
         .id(student.getId())
         .buduj();
+        mementoCaretaker.setMemento(studentDTO.createMemento());
         return studentDTO;
     }
     
@@ -82,6 +86,7 @@ public class StudenciApi {
             .id(student.getId())
             .buduj();
             studenciDTO.add(studentDTO);
+            mementoCaretaker.setMemento(studentDTO.createMemento());
         }
         return studenciDTO;
     }
@@ -98,6 +103,7 @@ public class StudenciApi {
                 .indeks(student.getIndeks())
                 .id(student.getId())
                 .buduj();
+                mementoCaretaker.setMemento(studentDTO.createMemento());
                 studenciDTO.add(studentDTO);
             }
     	}
@@ -111,6 +117,7 @@ public class StudenciApi {
                 .indeks(student.getIndeks())
                 .id(student.getId())
                 .buduj();
+                mementoCaretaker.setMemento(studentDTO.createMemento());
                 studenciDTO.add(studentDTO);
             }
     	}
@@ -138,9 +145,8 @@ public class StudenciApi {
         return false;
     }
     
-    public StudentDTO przywrocStudenta(int studentId) {
-        //TODO public StudentDTO przywrocStudenta(int studentId){
-        return null;
+    public StudentDTO przywrocStudenta(String indexNumber) {
+        return new StudentDTO(mementoCaretaker.getMementoOfStudent(indexNumber));
     }
 
 }
