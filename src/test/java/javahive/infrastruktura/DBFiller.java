15f7a6303fdf65b2d92bec2927f832bc5ce7ba05
michal-stankiewicz <michal.j.stankiewicz@gmail.com>
2014-06-25 09:18:18 +0200

    yamle podejsce 2 WIP

--- src/test/java/javahive/infrastruktura/DBFiller.java
+++ src/test/java/javahive/infrastruktura/DBFiller.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import com.fixy.Fixy;
 import com.fixy.JpaFixyBuilder;
 
+import javahive.domain.Ocena;
+import javahive.domain.Przedmiot;
+import javahive.domain.Wyklad;
 import javahive.domain.Wykladowca;
 
 import javax.annotation.PostConstruct;
@@ -32,36 +35,65 @@ public class DBFiller implements ApplicationContextAware{
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
 		Fixy fixtures = new JpaFixyBuilder(entityManager).withDefaultPackage("h2_TestData").useFieldAccess().build();
-		String stopien,imie,nazwisko;
-        /*addWykladowcaForTest(entityManager, "dr","Jacek","Krzaczkowski");
-        addWykladowcaForTest(entityManager, "prof. dr hab.","Pawel","Krzaczkowski");
-        addWykladowcaForTest(entityManager, "dr","Jacek","Krzaczkowski");
-        addWykladowcaForTest(entityManager, "dr","Jacek","Krzaczkowski");
-        addWykladowcaForTest(entityManager, "dr","Jacek","Krzaczkowski");
-		    
-		- Wykladowca(MIK):
-		    stopien: "prof. dr hab"
-		    imie: Paweł
-		    nazwisko: Mikołajczak
-		    
-		- Wykladowca(KAC):
-		    stopien: "dr hab."
-		    imie: Wiesława
-		    nazwisko: Kaczor
-		    
-		- Wykladowca(GOE):
-		    stopien: "prof. dr hab."
-		    imie: Kazimierz
-		    nazwisko: Goebel
-		*/
-		fixtures.load("h2_TestData/Studenci.yaml");
+		
+       
+		addOcenaForTest(entityManager, "2.0");
+        addOcenaForTest(entityManager, "2.5");
+        addOcenaForTest(entityManager, "3.0");
+        addOcenaForTest(entityManager, "3.5");
+        addOcenaForTest(entityManager, "4.0");
+        addOcenaForTest(entityManager, "4.5");
+        addOcenaForTest(entityManager, "5.0");
+		
+		addWykladowcy(entityManager);        
+        addPrzedmioty(entityManager);        
+        
+        addWykladForTest(entityManager, 1, 1); // ASD_KRZ
+        addWykladForTest(entityManager, 2, 2); // JAV_KUD
+        addWykladForTest(entityManager, 3, 3); // PRO_MIK
+        addWykladForTest(entityManager, 4, 3); // GRA_MIK
+        addWykladForTest(entityManager, 5, 4); // ANA_KAC
+        addWykladForTest(entityManager, 5, 5); // ANA_GOE
+        
+        /*fixtures.load("h2_TestData/Studenci.yaml");
 		fixtures.load("h2_TestData/Wykladowcy.yaml");
 		fixtures.load("h2_TestData/Przedmioty.yaml");
 		fixtures.load("h2_TestData/Oceny.yaml");
-        fixtures.load("h2_TestData/Indeksy.yaml");
+        fixtures.load("h2_TestData/Indeksy.yaml");*/
 		transaction.commit();
 		entityManager.close();
 	}
+    private void addPrzedmioty(EntityManager entityManager) {
+        addPrzedmiotForTest(entityManager, "Algorytmy i struktury danych");
+        addPrzedmiotForTest(entityManager, "Java - programowanie obiektowe");
+        addPrzedmiotForTest(entityManager, "Podstawy programowania");
+        addPrzedmiotForTest(entityManager, "Grafika komputerowa");
+        addPrzedmiotForTest(entityManager, "Analiza matematyczna");
+    }
+    private void addWykladowcy(EntityManager entityManager) {
+        addWykladowcaForTest(entityManager, "dr","Jacek","Krzaczkowski");
+        addWykladowcaForTest(entityManager, "dr","Rajmund","Kuduk");
+        addWykladowcaForTest(entityManager, "prof. dr hab.","Paweł","Mikołajczak");
+        addWykladowcaForTest(entityManager, "dr hab.","Wiesława","Kaczor");
+        addWykladowcaForTest(entityManager, "prof. dr hab.","Kazimierz","Goebel");
+    }
+    private void addWykladForTest(EntityManager entityManager, int przedmiotId,
+            int wykladowcaId) {
+        Wyklad w1 = new Wyklad();
+        w1.setPrzedmiot(entityManager.find(Przedmiot.class, przedmiotId));
+        w1.setWykladowca(entityManager.find(Wykladowca.class, wykladowcaId));
+        entityManager.persist(w1);
+    }
+    private void addPrzedmiotForTest(EntityManager entityManager, String nazwa) {
+        Przedmiot p1 = new Przedmiot();
+        p1.setNazwa(nazwa);
+        entityManager.persist(p1);
+    }
+    private void addOcenaForTest(EntityManager entityManager, String wysokosc) {
+        Ocena o1 = new Ocena();
+		o1.setWysokosc(wysokosc);
+		entityManager.persist(o1);
+    }
     private void addWykladowcaForTest(EntityManager entityManager,
             String stopien, String imie, String nazwisko) {
         Wykladowca w1 = new Wykladowca();
