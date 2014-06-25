dfc552dd38c734c59831258752cf3d42f94cda98
michal-stankiewicz <michal.j.stankiewicz@gmail.com>
2014-06-25 09:36:50 +0200

    yamle nadal WIP, indeksy i studenty do zrobienia

--- src/test/java/javahive/infrastruktura/DBFiller.java
+++ src/test/java/javahive/infrastruktura/DBFiller.java
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Component;
 import com.fixy.Fixy;
 import com.fixy.JpaFixyBuilder;
 
+import javahive.domain.Indeks;
 import javahive.domain.Ocena;
 import javahive.domain.Przedmiot;
 import javahive.domain.Wyklad;
 import javahive.domain.Wykladowca;
+import javahive.domain.Zaliczenie;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -37,32 +39,91 @@ public class DBFiller implements ApplicationContextAware{
 		Fixy fixtures = new JpaFixyBuilder(entityManager).withDefaultPackage("h2_TestData").useFieldAccess().build();
 		
        
-		addOcenaForTest(entityManager, "2.0");
+		addOceny(entityManager);		
+		addWykladowcy(entityManager);        
+        addPrzedmioty(entityManager);        
+        addWyklady(entityManager);
+        //addIndeks i studenty
+        addZaliczenia(entityManager);
+        
+		transaction.commit();
+		entityManager.close();
+	}
+    private void addOceny(EntityManager entityManager) {
+        addOcenaForTest(entityManager, "2.0");
         addOcenaForTest(entityManager, "2.5");
         addOcenaForTest(entityManager, "3.0");
         addOcenaForTest(entityManager, "3.5");
         addOcenaForTest(entityManager, "4.0");
         addOcenaForTest(entityManager, "4.5");
         addOcenaForTest(entityManager, "5.0");
-		
-		addWykladowcy(entityManager);        
-        addPrzedmioty(entityManager);        
-        
+    }
+    private void addWyklady(EntityManager entityManager) {
         addWykladForTest(entityManager, 1, 1); // ASD_KRZ
         addWykladForTest(entityManager, 2, 2); // JAV_KUD
         addWykladForTest(entityManager, 3, 3); // PRO_MIK
         addWykladForTest(entityManager, 4, 3); // GRA_MIK
         addWykladForTest(entityManager, 5, 4); // ANA_KAC
         addWykladForTest(entityManager, 5, 5); // ANA_GOE
-        
-        /*fixtures.load("h2_TestData/Studenci.yaml");
-		fixtures.load("h2_TestData/Wykladowcy.yaml");
-		fixtures.load("h2_TestData/Przedmioty.yaml");
-		fixtures.load("h2_TestData/Oceny.yaml");
-        fixtures.load("h2_TestData/Indeksy.yaml");*/
-		transaction.commit();
-		entityManager.close();
-	}
+    }
+    private void addZaliczenia(EntityManager entityManager) {
+        addZaliczenieForTest(entityManager,1 ,1 ,1 );
+        addZaliczenieForTest(entityManager,2 ,2 ,1 );
+        addZaliczenieForTest(entityManager,3 ,3 ,1 );
+        addZaliczenieForTest(entityManager,4 ,4 ,1 );
+        addZaliczenieForTest(entityManager,5 ,5 ,1 );
+        addZaliczenieForTest(entityManager,6 ,6 ,1 );
+        addZaliczenieForTest(entityManager,7 ,7 ,1 );
+        addZaliczenieForTest(entityManager,8 ,1 ,1 );
+        addZaliczenieForTest(entityManager,9 ,2 ,1 );
+        addZaliczenieForTest(entityManager,10 ,3 ,1 );
+        addZaliczenieForTest(entityManager,11 ,4 ,1 );
+        addZaliczenieForTest(entityManager,12 ,5 ,1 );
+        addZaliczenieForTest(entityManager,1 ,6 ,2 );
+        addZaliczenieForTest(entityManager,3 ,7 ,2 );
+        addZaliczenieForTest(entityManager,5 ,1 ,2 );
+        addZaliczenieForTest(entityManager,7 ,2 ,2 );
+        addZaliczenieForTest(entityManager,9 ,3 ,2 );
+        addZaliczenieForTest(entityManager,11 ,4 ,2 );
+        addZaliczenieForTest(entityManager,2 ,5 ,3 );
+        addZaliczenieForTest(entityManager,4 ,6 ,3 );
+        addZaliczenieForTest(entityManager,6 ,7 ,3 );
+        addZaliczenieForTest(entityManager,8 ,1 ,3 );
+        addZaliczenieForTest(entityManager,10 ,2 ,3 );
+        addZaliczenieForTest(entityManager,12 ,3 ,3 );
+        addZaliczenieForTest(entityManager,1 ,4 ,4 );
+        addZaliczenieForTest(entityManager,2 ,5 ,4 );
+        addZaliczenieForTest(entityManager,3 ,6 ,4 );
+        addZaliczenieForTest(entityManager,4 ,7 ,4 );
+        addZaliczenieForTest(entityManager,5 ,1 ,4 );
+        addZaliczenieForTest(entityManager,6 ,2 ,4 );
+        addZaliczenieForTest(entityManager,7 ,3 ,4 );
+        addZaliczenieForTest(entityManager,8 ,4 ,4 );
+        addZaliczenieForTest(entityManager,9 ,5 ,4 );
+        addZaliczenieForTest(entityManager,10 ,6 ,4 );
+        addZaliczenieForTest(entityManager,11 ,7 ,4 );
+        addZaliczenieForTest(entityManager,12 ,1 ,4 );
+        addZaliczenieForTest(entityManager,1 ,2 ,5 );
+        addZaliczenieForTest(entityManager,2 ,3 ,5);
+        addZaliczenieForTest(entityManager,3 ,4 ,5 );
+        addZaliczenieForTest(entityManager,4 ,5 ,5 );
+        addZaliczenieForTest(entityManager,5 ,6 ,5 );
+        addZaliczenieForTest(entityManager,6 ,7 ,5 );
+        addZaliczenieForTest(entityManager,7 ,1 ,6 );
+        addZaliczenieForTest(entityManager,8 ,2 ,6 );
+        addZaliczenieForTest(entityManager,9 ,3 ,6 );
+        addZaliczenieForTest(entityManager,10 ,4 ,6 );
+        addZaliczenieForTest(entityManager,11 ,5 ,6 );
+        addZaliczenieForTest(entityManager,12 ,6 ,6 );
+    }
+    private void addZaliczenieForTest(EntityManager entityManager,
+            int indeksId, int ocenaId, int wykladId) {
+        Zaliczenie z1 = new Zaliczenie();
+        z1.setIndeks(entityManager.find(Indeks.class, indeksId));
+        z1.setOcena(entityManager.find(Ocena.class, ocenaId));
+        z1.setWyklad(entityManager.find(Wyklad.class, wykladId));
+        entityManager.persist(z1);
+    }
     private void addPrzedmioty(EntityManager entityManager) {
         addPrzedmiotForTest(entityManager, "Algorytmy i struktury danych");
         addPrzedmiotForTest(entityManager, "Java - programowanie obiektowe");
